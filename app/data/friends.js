68862c2ea0afc3fb9a573bfb6d17f161bb218eec
Adriana Shulman <adriana33@mac.com>
2018-07-16 21:55:54 -0400

    update

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friends = [{
+        "name": "Alexis Bledel",
+        "photo": "../assets/alexis-bledel.jpg",
+        "scores": [
+            4,
+            3,
+            1,
+            1,
+            5,
+            4,
+            5,
+            1,
+            4,
+            2
+        ]
+    },
+    {
+        "name": "Milo Ventimiglia",
+        "photo": "../assets/milo-ventimiglia.jpg",
+        "scores": [
+            5,
+            1,
+            2,
+            2,
+            1,
+            3,
+            3,
+            1,
+            5,
+            2
+        ]
+    },
+    {
+        "name": "Matt Czuchry",
+        "photo": "../assets/matt-czuchry.jpg",
+        "scores": [
+            2,
+            1,
+            5,
+            5,
+            3,
+            1,
+            3,
+            3,
+            1,
+            2
+        ]
+    },
+    {
+        "name": "Sean Gunn",
+        "photo": "../assets/sean-gunn.jpg",
+        "scores": [
+            5,
+            5,
+            2,
+            3,
+            2,
+            1,
+            1,
+            4,
+            5,
+            1
+        ]
+    },
+    {
+        "name": "Liza Weil",
+        "photo": "../assets/liza-weil.jpg",
+        "scores": [
+            4,
+            2,
+            3,
+            3,
+            1,
+            5,
+            5,
+            1,
+            3,
+            3
+        ]
+    }
+];
+
+module.exports = friends;
+
